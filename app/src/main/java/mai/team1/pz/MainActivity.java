b324aa2047be1732a3c55b6c4319f492865f1192
JolyGot <kuznetsov_akn@mail.ru>
2023-11-13 20:32:58 +0300

    Выбор элемента в ListView

--- app/src/main/java/mai/team1/pz/MainActivity.java
+++ app/src/main/java/mai/team1/pz/MainActivity.java
@@ -3,12 +3,46 @@ package mai.team1.pz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] countries = { "Бразилия", "Аргентина", "Колумбия", "Чили", "Уругвай"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView selection = findViewById(R.id.selection);
+
+        ListView countriesList = findViewById(R.id.countriesList);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_multiple_choice, countries);
+
+        countriesList.setAdapter(adapter);
+
+        countriesList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+            {
+                SparseBooleanArray selected=countriesList.getCheckedItemPositions();
+
+                String selectedItems="";
+                for(int i=0;i < countries.length;i++)
+                {
+                    if(selected.get(i))
+                        selectedItems+=countries[i]+",";
+                }
+                // установка текста элемента TextView
+                selection.setText("Выбрано: " + selectedItems);
+            }
+        });
     }
 }
