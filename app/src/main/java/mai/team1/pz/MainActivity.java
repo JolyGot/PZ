88dbb54e8d56154e5e6bf7e8508f86c6e7fc12f7
JolyGot <kuznetsov_akn@mail.ru>
2023-11-13 20:23:12 +0300

    ListView и ArrayAdapter

--- app/src/main/java/mai/team1/pz/MainActivity.java
+++ app/src/main/java/mai/team1/pz/MainActivity.java
@@ -3,12 +3,27 @@ package mai.team1.pz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    String[] countries = { "Бразилия", "Аргентина", "Колумбия", "Чили", "Уругвай"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ListView countriesList = findViewById(R.id.countriesList);
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, countries);
+
+
+        countriesList.setAdapter(adapter);
     }
 }
